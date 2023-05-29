8ca8d4ffadf662533910528c3af15a6e2cd5fcfd
da_lugh <91050093+GHzin@users.noreply.github.com>
2023-05-29 17:25:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var botaoProdutos = document.getElementById("butom-produtos");
-var produtoBar = document.getElementById("produtos");
-var seta = document.getElementById("arrow");
+var detalheProdutos = document.getElementById("detalhe-produtos-bg");
+var body = document.getElementById("body")
 
-var exibindo = false;
+function openDetalhes(){
+  detalheProdutos.style.display = 'flex'
+  body.style.overflowY = "hidden";
+}
 
-botaoProdutos.addEventListener("click", (event) => {
-  if (!exibindo) {
-    produtoBar.style.display = 'flex';
-    produtoBar.style.flexDirection = 'column';
-    seta.style.transform = 'rotate(225deg)';
-    exibindo = true;
-  } else {
-    produtoBar.style.display = 'none';
-    seta.style.transform = 'rotate(45deg)';
-    exibindo = false;
+function closeDetalhes(){
+  detalheProdutos.style.display = 'none'
+  body.style.overflowY = "scroll";
+}
+
+
+detalheProdutos.addEventListener("click", function(event){
+  if(event.srcElement.id == "detalhe-produtos-bg"){
+    closeDetalhes()
+  
   }
-});
+})
